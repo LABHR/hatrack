421b56924224f410c6ffd5e63fe19afb343e970c
Lee Symes <leesdolphin@gmail.com>
2016-02-11 02:18:17 +1300

    Refactored code to allow progress notifications.
     - Refactored away jQuery from internals so it can be replaced if desired.
     - Improved speed by loading comments as the issues are loaded instead of
        after all of the issues completely load.
     - Improved visible speed by allowing results to be returned to the user
        whilst the whole data set is still loading.
     - Moved progress spinner code around to make life a bit easier if we
       decide to move the script block.

---
+++ README.md
@@ -0,0 +1,25 @@
+Let's All Build a Hat Rack -- JavaScript
+========================================
+
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+
+To run, simply open `index.html`.
+
+To run the linter install the dependancies using `npm install` and then run the linter `npm run lint`
+
+TODO
+----
+
+ [ ] Handle errors in some graceful way.
+ [ ] Be able to display data whilst it is loading.
+ [ ] Download issue information faster. Specifically once a single issue page is downloaded, start
+      downloading the comment lists.
+ [ ] Remove all direct dependancies on jQuery, simply rely on 1 or 2 externally defined functions to
+      allow the implementer to choose their own libraries.
+ [ ] Implement a cache that persists across page loads. Currently the browser caches most requests,
+      however if we were able to have a persistent cache we could implement more aggressive caching
+      such as the kinds described on
+      [this issue](https://github.com/LABHR/octohatrack/issues/34#issuecomment-182282194)
+ [ ] Add support for restricting the number of issues retrieved. Either through a form field or
+      after the first issues request popping up a notification along the lines of 'Too many issues
+      to comb through, how about we limit it to the last year?'.
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-;(function ($) {
-  // var cache = {}
+(function ($) {
   var linkRe = /<([^>]+)>; rel="(\w+)"/
-
   function combine () {
     var arr = []
     Array.prototype.forEach.call(arguments, function (arg) {
@@ -9,22 +7,6 @@
     })
     return arr
   }
-  function combineAll (arrayOfArrays) {
-    var arr = []
-    Array.prototype.forEach.call(arrayOfArrays, function (innerArray) {
-      Array.prototype.push.apply(arr, innerArray)
-    })
-    return arr
-  }
-  function whenAll (arrayOfPromises) {
-    // A warapper around $.when that takes an array and returns a promise the resolves with an
-    //  array.
-    return $.when.apply($, arrayOfPromises).then(function () {
-      // Convert the arguments into an array.
-      return Array.prototype.slice.call(arguments)
-    })
-  }
-
   function parseLinks (linkHeader) {
     var links = {}
     linkHeader.split(', ').forEach(function (linkPart) {
@@ -37,155 +19,241 @@
     })
     return links
   }
-  // Labhr.prototype.credentialSetup = function (xhr, settings) {
-  //   xhr.withCredentials = true
-  //   xhr.setRequestHeader('Authorization', 'Basic <<Your Auth Here!>>')
-  // }
-  //
-  // Labhr.prototype.cachedRequest = function (url) {
-  //   if (cache[url]) {
-  //     return cache[url]
-  //   } else {
-  //     var response = cache[url] = $.ajax(url, {
-  //       method: 'GET',
-  //       beforeSend: this.credentialSetup
-  //     })
-  //     response.then(function (json, status, response) {
-  //       //
-  //     })
-  //     return response
-  //   }
-  // }
+  function filterContributors (listOfContributors, usernameStore) {
+    var filteredList = []
+    // Only send unique contributors out.
+    listOfContributors.forEach(function (contributor) {
+      if (!usernameStore[contributor.login]) {
+        usernameStore[contributor.login] = true
+        filteredList.push(contributor)
+      }
+    })
+    return filteredList
+  }
+  // Functions using jQuery.
 
-  Labhr.prototype.doPagination = function (sharedCache, url, direction) {
+  function whenAll (arrayOfPromises) {
+    // A warapper around $.when that takes an array and returns a promise the resolves with an
+    //  array.
+    return $.when.apply($, arrayOfPromises).then(function () {
+      // Convert the arguments into an array.
+      return Array.prototype.slice.call(arguments)
+    })
+  }
+  function when (/* ... */) {
+    return $.when.apply($, arguments)
+  }
+  function resolvedPromise (value) {
+    // It's hacky but it appears to be the best way to get a resolved promise.
+    return $.Deferred().resolve(value).promise()
+  }
+
+  function Labhr (host, cachedRequest) {
+    if (this.constructor !== Labhr) {
+      // Handle people calling this function without 'new'.
+      return new Labhr(host, cachedRequest)
+    }
+    this.host = host
+    this.cachedRequest = cachedRequest
+  }
+  window.Labhr = Labhr
+
+  Labhr.prototype.load = function (repo, notifyCallback) {
+    var codeContribUsernames = {}
+    var allCodeContributors = []
+    var nonCodeContribUsernames = {}
+    var allNonCodeContributors = []
+    var allNonCodeOnlyContributors = []
+    function codeContribCB (listOfCodeContributors) {
+      var codeContribs = filterContributors(listOfCodeContributors,
+                                            codeContribUsernames)
+      allCodeContributors = combine(allCodeContributors, codeContribs)
+      notifyCallback({
+        'codeContributors': codeContribs
+      })
+    }
+    function nonCodeContribCB (listOfNonCodeContributors) {
+      var nonCodeContribs = filterContributors(listOfNonCodeContributors,
+                                               nonCodeContribUsernames)
+      allNonCodeContributors = combine(allNonCodeContributors, nonCodeContribs)
+      // Make a copy in case notifyCallback does something horrible.
+      var nonCodeContribs_copy = nonCodeContribs.slice(0)
+      // This list is for all contributors who have worked on issues(or PRs).
+      // There may be overlap between this list and the code contributors list.
+      notifyCallback({
+        'nonCodeContributors': nonCodeContribs
+      })
+      // We need to wait until we have all the code contributors before
+      //  sending the non-code only contributors.
+      listOfCodeContributors.then(function () {
+        // Remove any contributors who are code contributors
+        var nonCodeOnlyContribs = filterContributors(nonCodeContribs_copy,
+                                                     codeContribUsernames)
+        allNonCodeOnlyContributors = combine(allNonCodeOnlyContributors,
+                                             nonCodeOnlyContribs)
+        notifyCallback({
+          'nonCodeOnlyContributors': nonCodeOnlyContribs
+        })
+      })
+    }
+    var listOfCodeContributors = this.codeContributionLoader(repo, codeContribCB)
+    return when(
+      listOfCodeContributors,
+      this.issueContributionLoader(repo, nonCodeContribCB)
+    ).then(function (codeContrib, issueContrib) {
+      return {
+        'nonCodeContributors': allNonCodeContributors,
+        'nonCodeOnlyContributors': allNonCodeOnlyContributors,
+        'codeContributors': allCodeContributors
+      }
+    })
+  }
+
+  // Loading extended user information
+
+  Labhr.prototype.getUserExtendedInfo = function (userInfo) {
+    return this.cachedRequest(userInfo.url).then(function (json, status, xhr) {
+      return json
+    })
+  }
+
+  Labhr.prototype.getAllUserExtendedInfo = function (listOfUsers) {
+    var that = this
+    var allPromises = []
+    listOfUsers.forEach(function (user) {
+      allPromises.push(that.getUserExtendedInfo(user))
+    })
+    // Return a promise that, when resolved, will give an array containing
+    //  all the users and their extending information.
+    return whenAll(allPromises)
+  }
+
+  // Loading the users from contributors list and issues.
+
+  Labhr.prototype.codeContributionLoader = function (repo, contributorCallback) {
+    /**
+     * repo: A string containing the repo to load the contributors from.
+     * contributorCallback: A function that recieves a list of contributors.
+     *                      This list may not be complete or unique.
+     */
+    var that = this
+    var baseUrl = this.host + '/repos/' + repo + '/contributors?pageSize=30'
+    return this.autoPaginate(baseUrl, function (listOfContributors) {
+      return that.getAllUserExtendedInfo(listOfContributors).then(contributorCallback)
+    })
+  }
+
+  Labhr.prototype.issueContributionLoader = function (repo, contributorCallback) {
+    /**
+     * repo: A string containing the repo to load the contributors from.
+     * contributorCallback: A function that recieves a list of contributors.
+     *                      This list may not be complete or unique.
+     */
+    var that = this
+    var baseUrl = this.host + '/repos/' + repo + '/issues?pageSize=30&state=all'
+    return this.autoPaginate(baseUrl, function (listOfIssues) {
+      // We need to do 2 things with this list of issues.
+      //  - Load the comments for each issue
+      //  - Load the user that created the issue.
+      var creators = []
+      var commentLoadingPromises = []
+      listOfIssues.forEach(function (issue) {
+        creators.push(issue.user)
+        commentLoadingPromises.push(
+          that._issueCommenterLoader(issue.comments_url, contributorCallback))
+      })
+      var creatorsPromise = that.getAllUserExtendedInfo(creators).then(contributorCallback)
+      var commentsPromise = whenAll(commentLoadingPromises)
+      return when(creatorsPromise, commentsPromise)
+    })
+  }
+
+  Labhr.prototype._issueCommenterLoader = function (commentsUrl, contributorCallback) {
+    /**
+     * An internal method to load all the commenters from a single issue.
+     *
+     * repo: A string containing the repo to load the contributors from.
+     * contributorCallback: A function that recieves a list of contributors.
+     *                      This list may not be complete or unique.
+     */
+    var that = this
+    return this.autoPaginate(commentsUrl, function (commentsPage) {
+      var commenters = []
+      commentsPage.forEach(function (comment) {
+        commenters.push(comment.user)
+      })
+      return that.getAllUserExtendedInfo(commenters).then(function (fullUserInformation) {
+        // Once we load the information, pass it on to the callback.
+        contributorCallback(fullUserInformation)
+      })
+    })
+  }
+
+  // Pagination helpers.
+
+  Labhr.prototype.doPagination = function (sharedCache, url, direction, requestParser) {
+    /**
+     * Handles recursively fetching pages in the direction given.
+     *
+     * sharedCache: A dictionary that allows multiple `doPagination` calls to
+     *  work alongside each other without accidently duplicating items in the combined results.
+     */
     var that = this
     if (!url || sharedCache[url]) {
-      return $.Deferred().resolve([]).promise()
+      // This can probably be done nicer.
+      return resolvedPromise([])
     } else {
       sharedCache[url] = true
       return this.cachedRequest(url).then(function (json, status, response) {
         var links = parseLinks(response.getResponseHeader('Link'))
         var next_url = links[direction]
-        return that.doPagination(sharedCache, next_url, direction).then(
-          function (runningPagination) {
-            if (direction === 'next') {
-              return combine(json, runningPagination)
-            } else {
-              return combine(runningPagination, json)
-            }
-          })
+        return when(
+          requestParser(json),
+          that.doPagination(sharedCache, next_url, direction)
+        )
       })
     }
   }
 
-  Labhr.prototype.autoPaginate = function (baseUrl) {
+  Labhr.prototype.autoPaginate = function (baseUrl, requestParser) {
+    /**
+     * baseUrl: The first URL to request. Other page's URLs are taken from the Link header.
+     * requestParser: A function that performs some additional processing and
+     *                returns a promise or an object. Returned promises will
+     *                be waited on before this function returns.
+     */
+    requestParser = requestParser || function (request) { return request }
     var that = this
     return that.cachedRequest(baseUrl).then(function (json, status, response) {
       var linkHeader = response.getResponseHeader('Link')
       if (linkHeader) {
         var links = parseLinks(linkHeader)
         var paginationCache = {}
-        return $.when(
-          that.doPagination(paginationCache, links['next'], 'next'),
-          that.doPagination(paginationCache, links['last'], 'prev')
-        ).then(function (list1, list2) {
-          return combine(json, list1, list2)
-        })
-      } else {
-        return json
-      }
-    })
-  }
-
-  Labhr.prototype.getUserFullInfo = function (userInfo) {
-    return this.cachedRequest(userInfo.url).then(function (json, status, xhr) {
-      return json
-    })
-  }
-
-  Labhr.prototype.getAllUserFullInfo = function (listOfUsers) {
-    var that = this
-    var allPromises = []
-    listOfUsers.forEach(function (user) {
-      allPromises.push(that.getUserFullInfo(user))
-    })
-    return whenAll(allPromises).then(function (allUsers) {
-      return allUsers
-    })
-  }
-
-  Labhr.prototype.codeContributors = function (host, repo) {
-    var that = this
-    var baseUrl = host + '/repos/' + repo + '/contributors?pageSize=30'
-    return this.autoPaginate(baseUrl).then(function (users) {
-      return that.getAllUserFullInfo(users)
-    })
-  }
-
-  Labhr.prototype.issueContributors = function (host, repo) {
-    var that = this
-    // This API call also includes Pull requests(I think) ... sweet.
-    var baseUrl = host + '/repos/' + repo + '/issues?pageSize=30&state=all'
-    return this.autoPaginate(baseUrl).then(function (issues) {
-      var perIssuePromises = []
-      issues.forEach(function (issue) {
-        perIssuePromises.push(
-          that.autoPaginate(issue.comments_url).then(function (allComments) {
-            var issueUsers = [issue.user]
-            allComments.forEach(function (comment) {
-              issueUsers.push(comment.user)
-            })
-            return that.getAllUserFullInfo(issueUsers)
-          })
+        return when(
+          requestParser(json),
+          that.doPagination(paginationCache, links['next'], 'next', requestParser),
+          that.doPagination(paginationCache, links['last'], 'prev', requestParser)
         )
-      })
-      return whenAll(perIssuePromises).then(function (issueUsers) {
-        return combineAll(issueUsers)
-      }).then(function (allUsers) {
-        var issueUserLogins = {}
-        var finalList = []
-        allUsers.forEach(function (user) {
-          if (!issueUserLogins[user.login]) {
-            issueUserLogins[user.login] = true
-            finalList.push(user)
-          }
-        })
-        return finalList
-      })
-    })
-  }
-
-  function Labhr (host, repo, cachedRequest) {
-    if (this.constructor != Labhr) {
-      return new Labhr(host, repo, cachedRequest)
-    }
-    this.host = host
-    this.repo = repo
-    this.cachedRequest = cachedRequest
-  }
-
-  Labhr.prototype.load = function () {
-    return $.when(
-      this.codeContributors(this.host, this.repo),
-      this.issueContributors(this.host, this.repo)
-    ).then(function (codeContrib, issueContrib) {
-      var codeContribUsernames = {}
-      codeContrib.forEach(function (user) {
-        codeContribUsernames[user.login] = true
-      })
-      var issueOnlyContrib = []
-      issueContrib.forEach(function (user) {
-        if (!codeContribUsernames[user.login]) {
-          issueOnlyContrib.push(user)
-        }
-      })
-      return {
-        issuesContributors: issueContrib,
-        issuesOnlyContributors: issueOnlyContrib,
-        codeContributors: codeContrib
+      } else {
+        return requestParser(json)
       }
     })
   }
+})(window.jQuery)
 
-  window.Labhr = Labhr
-})(jQuery)
+// Example `cachedRequest` implementation using jQuery.
+//
+// var cache = {}
+// function cachedRequest (url) {
+//   if (cache[url]) {
+//     return cache[url]
+//   } else {
+//     var response = cache[url] = $.ajax(url, {
+//       method: 'GET',
+//       headers: {
+//         'Authorization': 'Basic <<Your Auth Here!>>'
+//       }
+//     })
+//     return response
+//   }
+// }
